0f7ef446189813e0c8a008ec56a1752e07417985
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-04-10 03:05:44 -0500

    feat: Add ClassroomController API endpoint to list avaible classrooms

---
+++ src/main/java/com/backend/IMonitoring/controller/ClassroomController.java
@@ -0,0 +1,29 @@
+package com.backend.IMonitoring.controller;
+
+import com.backend.IMonitoring.model.Classroom;
+import com.backend.IMonitoring.model.ClassroomType;
+import com.backend.IMonitoring.service.ClassroomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/classrooms")
+@RequiredArgsConstructor
+public class ClassroomController {
+    private final ClassroomService classroomService;
+
+    @GetMapping("/available")
+    public ResponseEntity<List<Classroom>> getAvailableClassrooms(
+            @RequestParam LocalDateTime start,
+            @RequestParam LocalDateTime end,
+            @RequestParam String building,
+            @RequestParam ClassroomType type
+    ) {
+        return ResponseEntity.ok(
+                classroomService.getAvailableClassrooms(start, end, building, type)
+        );
+    }
+}
