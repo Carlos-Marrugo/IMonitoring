373acd29cd4a91bad8eb30079b6cc20bc05f8cbd
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-04-10 03:31:38 -0500

    refactor: queries for avaible, not avaible and id room search

--- src/main/java/com/backend/IMonitoring/repository/ClassroomRepository.java
+++ src/main/java/com/backend/IMonitoring/repository/ClassroomRepository.java
@@ -10,15 +10,30 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, String> {
-    @Query("SELECT c FROM Classroom c WHERE " +
-            "c.building.name = :building AND c.type = :type AND " +
-            "c NOT IN (SELECT r.classroom FROM Reservation r WHERE " +
+
+    // sala dispo
+    @Query("SELECT c FROM Classroom c WHERE c NOT IN " +
+            "(SELECT r.classroom FROM Reservation r WHERE " +
             "r.status = 'CONFIRMADA' AND " +
-            "(r.startTime < :end AND r.endTime > :start))")
-    List<Classroom> findAvailableClassrooms(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end,
-            @Param("building") String building,
-            @Param("type") ClassroomType type
+            "(r.startTime < CURRENT_TIMESTAMP AND r.endTime > CURRENT_TIMESTAMP))")
+    List<Classroom> findAvailableNow();
+
+    // salas no disponibles en tiempo actual
+    @Query("SELECT c FROM Classroom c WHERE c IN " +
+            "(SELECT r.classroom FROM Reservation r WHERE " +
+            "r.status = 'CONFIRMADA' AND " +
+            "(r.startTime < CURRENT_TIMESTAMP AND r.endTime > CURRENT_TIMESTAMP))")
+    List<Classroom> findUnavailableNow();
+
+    // disponibildiad de sala en especifica
+    @Query("SELECT CASE WHEN COUNT(r) = 0 THEN true ELSE false END " +
+            "FROM Reservation r WHERE " +
+            "r.classroom.id = :classroomId AND " +
+            "r.status = 'CONFIRMADA' AND " +
+            "(r.startTime < :endTime AND r.endTime > :startTime)")
+    boolean isAvailable(
+            @Param("classroomId") String classroomId,
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
     );
 }
